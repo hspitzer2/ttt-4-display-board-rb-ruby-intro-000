26fc8e6fd5e1ea2e57aa36181eff3971dd104095
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-10 20:17:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,15 +6,13 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
- board = variousGameStiuations
+ board = variousGameSituations
  puts "#{0}", "#{1}","#{2}","#{3}","#{4}","#{5}","#{6}","#{7}","#{8}"
-display_board(board)
 
 def capture_puts
   output
 end
 
-output = rows
-rows = baord
-
-puts board[]
+variousGameSituations
+ puts "#{0}", "#{1}","#{2}","#{3}","#{4}","#{5}","#{6}","#{7}","#{8}"
+ puts "#{0}", "#{1}","#{2}","#{3}","#{4}","#{5}","#{6}","#{7}","#{8}"
