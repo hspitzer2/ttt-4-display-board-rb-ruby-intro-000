971a4122ece8edeb2be4921d5e76d533b240eb2c
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-10 21:00:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,7 +11,7 @@ def capture_puts
   end
 end
  board = variousGameSituations
- puts ["#{0}", "#{1}","#{2}","#{3}","#{4}","#{5}","#{6}","#{7}","#{8}"]
+ puts ["#{0}","#{1}","#{2}","#{3}","#{4}","#{5}","#{6}","#{7}","#{8}"]
 
 variousGameSituations
 board[4] ="X"
